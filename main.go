9c636dcbf5e7131dd816264bef0d3ffd34107fc1
Gunzilla <joshuahauhio@gmail.com>
2021-07-06 00:07:15 -0500

    h

--- main.go
+++ main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Gunzilla89/goBasics/myFunctions"
 )
 
 func main() {
 	fmt.Println("Hello World")
 	x := 2
 	y := 3
-
-	variable := myFunctions.Add(x, y)
-
-	fmt.Println(variable)
 
 }
